3187caa207236bfd06b5b84f6773a42d6f572d4e
Hafeez <muhammed.hafeez.nayazi@gmail.com>
2024-09-08 14:01:22 +0530

    fixed deployment bug

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,15 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
 const app = express();
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join("public")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join("public", "index.html"), { root: "./" });
 });
 
 // if development
-// app.listen(80, () => {
-//     console.log('http://localhost:80');
-   
-// })
+app.listen(80, () => {
+  console.log("http://localhost:80");
+});
 
 //if production
 module.exports = app;
