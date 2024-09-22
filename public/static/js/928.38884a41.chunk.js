ff97eb7a8f0eac3e4391fe8fc6a0bec08edb0c09
Hafeez <muhammed.hafeez.nayazi@gmail.com>
2024-09-23 01:03:05 +0530

    added build

---
+++ public/static/js/928.38884a41.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[928],{928:(n,e,s)=>{s.r(e),s.d(e,{default:()=>a});s(43);var t=s(579);const a=function(){return(0,t.jsx)("div",{className:"Analytics"})}}}]);
+//# sourceMappingURL=928.38884a41.chunk.js.map
