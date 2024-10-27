037d1203fd0dc016ac1b449b4baf1e885adf8926
Hafeez <muhammed.hafeez.nayazi@gmail.com>
2024-10-27 14:13:12 +0530

    removed the port for https requests from the client

--- frontend/src/services/analytics.js
+++ frontend/src/services/analytics.js
@@ -1,8 +1,7 @@
 import apiRequest from "../utils/fetchApi";
 const protocol = window.location.protocol; // e.g., 'http:' or 'https:'
 const host = window.location.hostname; // e.g., 'localhost' or 'example.com'
-const port = 80; // e.g., '3000', '80', or '' (empty for default ports)
-const baseURL = `${protocol}//${host}:${port}`;
+const baseURL = `${protocol}//${host}`;
 export async function getLeads() {
   const result = await apiRequest(baseURL + "/api/analytics/leads", "GET");
   if (result.success === false) {
--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -3,8 +3,7 @@ import apiRequest from "../utils/fetchApi";
 // import apiRequest from "../utils/fetchApi";
 const protocol = window.location.protocol; // e.g., 'http:' or 'https:'
 const host = window.location.hostname; // e.g., 'localhost' or 'example.com'
-const port = 80; // e.g., '3000', '80', or '' (empty for default ports)
-const baseURL = `${protocol}//${host}:${port}`;
+const baseURL = `${protocol}//${host}`;
 
 export async function login(email, password) {
   try {
